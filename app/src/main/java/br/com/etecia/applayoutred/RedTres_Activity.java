6fa1e86f16190199ea87f9fbd5b34e73f60f6751
Emily Lemos <emily.lemos@etec.sp.gov.br>
2019-09-04 22:08:37 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/applayoutred/RedTres_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.applayoutred;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RedTres_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.red_tres_layout);
+    }
+}
